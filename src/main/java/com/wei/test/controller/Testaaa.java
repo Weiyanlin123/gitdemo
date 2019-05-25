db1045ea893e11cd879ea89a618a7c46a6b2396c
Weiyanlin123 <17621257791@163.com>
2019-05-25 16:31:15 +0800

    nihao

--- src/main/java/com/wei/test/controller/Testaaa.java
+++ src/main/java/com/wei/test/controller/Testaaa.java
@@ -5,9 +5,12 @@ public class Testaaa {
     private  int bbb=3;
     private  String cc="zhugan";
     private String ccc="主干提交分支";
-    
-    
+
+
     private String ddd="nihao";
+
+
+    private String nihao="nihao";
     public String getAaa() {
         return aaa;
     }
