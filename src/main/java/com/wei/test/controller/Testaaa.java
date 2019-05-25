06f9d91936f76a0e432135dc4d1a69c950235221
Weiyanlin123 <17621257791@163.com>
2019-05-25 16:12:51 +0800

    后端

--- src/main/java/com/wei/test/controller/Testaaa.java
+++ src/main/java/com/wei/test/controller/Testaaa.java
@@ -3,6 +3,7 @@ package com.wei.test.controller;
 public class Testaaa {
     private String  aaa="";
     private  int bbb=3;
+    private  String cc="zhugan";
     public String getAaa() {
         return aaa;
     }
