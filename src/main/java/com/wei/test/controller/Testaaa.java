b47ffb164e80c53ff01468f87d6920cf136aeb96
Weiyanlin123 <17621257791@163.com>
2019-05-25 16:18:00 +0800

    后端2

--- src/main/java/com/wei/test/controller/Testaaa.java
+++ src/main/java/com/wei/test/controller/Testaaa.java
@@ -4,6 +4,7 @@ public class Testaaa {
     private String  aaa="";
     private  int bbb=3;
     private  String cc="zhugan";
+    private String ccc="主干提交分支";
     public String getAaa() {
         return aaa;
     }
